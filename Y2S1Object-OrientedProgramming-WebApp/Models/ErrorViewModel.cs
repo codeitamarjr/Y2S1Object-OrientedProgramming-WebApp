d167d39b1d180f96a216bb85af8932298f73d339
Itamar Junior <itjunior@gmail.com>
2021-10-19 16:25:18 +0100

    first comid

---
+++ Y2S1Object-OrientedProgramming-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Y2S1Object_OrientedProgramming_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
